290948454d6ace067eb05e82611df812ad5143c0
Knight-eGithub <githubknight@gmail.com>
2025-11-14 14:12:49 +0530

    make reuseable ui component home category card

---
+++ components/ui/CategoryCard.jsx
@@ -0,0 +1,32 @@
+import RenderCategoryCard from "./CatergoryCard/RenderCategoryCard";
+import EggHome from "./CatergoryCard/FontPath/EggHome";
+import BakeryHome from "./CatergoryCard/FontPath/BakeryHome";
+import VegetableHome from "./CatergoryCard/FontPath/VegetableHome";
+import { BakeryBgCard, EggBgCard, VegetableBgCard } from "./CatergoryCard/sections/BgCard";
+import { EggBtCard, VegetableBtCard, BakeryBtCard } from "./CatergoryCard/sections/BottomCard";
+
+export default function CategoryCard({category}) {
+    const categories = {
+        "vegetable": {
+            imageSrc: "/vegetable-home.png",
+            py: "70", w: "305.606", h: "204.426",
+            bgRect: <VegetableBgCard />,
+            titlePath: <VegetableHome />,
+            bottomCard: <VegetableBtCard/>
+        },
+        "bakery": {
+            imageSrc: "/bakery-home.png",
+            bgRect: <BakeryBgCard/>,
+            py: "15",
+            titlePath: <BakeryHome />,
+            bottomCard: <BakeryBtCard />
+        },
+        "eggs": {
+            imageSrc: "/eggs-home.png",
+            bgRect: <EggBgCard/>,
+            titlePath: <EggHome />,
+            bottomCard: <EggBtCard />
+        },
+    };
+    return <RenderCategoryCard {...categories[category]} />
+}
