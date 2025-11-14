6511e0a535ab6f2c1c37933bb5d5469bc57e9715
Knight-eGithub <githubknight@gmail.com>
2025-11-14 16:36:46 +0530

    change from jsx to tsx

--- components/ui/CategoryCard.tsx
+++ components/ui/CategoryCard.tsx
@@ -5,28 +5,35 @@ import VegetableHome from "./CatergoryCard/FontPath/VegetableHome";
 import { BakeryBgCard, EggBgCard, VegetableBgCard } from "./CatergoryCard/sections/BgCard";
 import { EggBtCard, VegetableBtCard, BakeryBtCard } from "./CatergoryCard/sections/BottomCard";
 
-export default function CategoryCard({category}) {
+interface CategoryCardProps {
+    category: "vegetable" | "bakery" | "eggs";
+}
+
+export default function CategoryCard({ category }: CategoryCardProps) {
     const categories = {
-        "vegetable": {
+        vegetable: {
             imageSrc: "/vegetable-home.png",
-            py: "70", w: "305.606", h: "204.426",
+            py: "70",
+            w: "305.606",
+            h: "204.426",
             bgRect: <VegetableBgCard />,
             titlePath: <VegetableHome />,
-            bottomCard: <VegetableBtCard/>
+            bottomCard: <VegetableBtCard />,
         },
-        "bakery": {
+        bakery: {
             imageSrc: "/bakery-home.png",
-            bgRect: <BakeryBgCard/>,
+            bgRect: <BakeryBgCard />,
             py: "15",
             titlePath: <BakeryHome />,
-            bottomCard: <BakeryBtCard />
+            bottomCard: <BakeryBtCard />,
         },
-        "eggs": {
+        eggs: {
             imageSrc: "/eggs-home.png",
-            bgRect: <EggBgCard/>,
+            bgRect: <EggBgCard />,
             titlePath: <EggHome />,
-            bottomCard: <EggBtCard />
+            bottomCard: <EggBtCard />,
         },
     };
-    return <RenderCategoryCard {...categories[category]} />
+
+    return <RenderCategoryCard {...categories[category]} />;
 }
